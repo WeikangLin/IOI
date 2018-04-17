aa24e01bc3302aec6ceb9bd80b301a847af63e91
Weikang Lin <weikanglin@cometnet-10-21-45-132.utdallas.edu>
2018-04-17 12:02:27 -0500

    Add ./batch as an example

--- IOI_arbitary.m
+++ IOI_arbitary.m
@@ -1,31 +1,73 @@
-%%%%%% two-experiment IOI for arbitary number of contraints
-%%%%%% and in arbitary parameter space
+%%%%%% A script for CosmoMC (after running getdist)
+%%%%%% Calculate two-experiment IOIs for arbitary number of contraints
+%%%%%% and in an arbitary parameter space
 %%%%%% outputs in e.g., IOI_CMB.txt
+%%%%%% Steps:
+%%%%%% 0. Put all the .margestats and .corr files of the constraints of
+%%%%%%    interest in one folder.
+%%%%%% 1. Specify the constraint files directory that contains all 
+%%%%%%    the .margestats and .corr files.
+%%%%%% 2. Put the parameter names below 
+%%%%%%    e.g., H0 parameterization in LCDM model:
+%%%%%%    Params = {'omegabh2','omegam','H0','sigma8','ns','tau'};
+%%%%%%    e.g., Theta parameterization in LCDM model:
+%%%%%%    Params = {'omegabh2','omegach2','theta','logA','ns','tau'}; 
+
 clear;
+Params = {'omegabh2','omegach2','theta','logA','ns'};
+constraint_filedir = './batch/';
 
-%%%%%% Put below all constraints to be compared:
-exp_names = {'hiTT_lowTEB' ,'hiTE_lowTEB','hiEE_lowTEB'};
+Outfile = 'IOIs.txt';
 
-%%%%%% Put the parameter names below 
-% e.g., H0 parameterization in LCDM model:
-%Params = {'omegabh2','omegam','H0','sigma8','ns','tau'};
-%%%%%% e.g., Theta parameterization in LCDM model:
-Params = {'omegabh2','omegach2','theta','logA','ns','tau'};
-
+margfiles = dir(fullfile(constraint_filedir, '*.margestats'));
+Num_exp = length(margfiles);
 ParamDim = length(Params);
 
-Num_exp = length(exp_names);
+exp_names = string(Num_exp);
+for i=1:Num_exp
+    exp_names(i) = erase(margfiles(i).name,'.margestats');
+end
 
-delimiterIn = ' ';
-headerlinesIn = 1;
+Message = sprintf([num2str(Num_exp) ' constraints, ' ...
+    num2str(ParamDim) ' parameters.']);
+disp(Message)
 
+%%%%%% Extract mu and C from files
 C = zeros(ParamDim,ParamDim,Num_exp);
 mu = zeros(ParamDim,Num_exp);
 sigma = zeros(ParamDim,Num_exp);
 index = zeros(ParamDim,1);
 
+delimiterIn = ' ';
+headerlinesIn = 1;
+
 for i = 1:Num_exp
-    Corr = importdata(['../batch/' exp_names{i} '.corr']);
+    for k = 1:ParamDim
+        fileID = fopen([constraint_filedir margfiles(i).name]);
+        Marg_header = textscan(fileID,'%s',13);
+        index(k) = 1;
+        NotFound = true;
+        for j = 1:400
+            meat = textscan(fileID,'%s %f %f %*[^\n]',1);
+            str =meat{1};
+            if (strcmp(str,Params{k}) == 1|strcmp(str,[Params{k},'*']) == 1)
+                NotFound = false;
+                break
+            end
+            index(k) = index(k)+1;
+        end
+        if NotFound
+            error = sprintf(['Error: \n' ...
+                Params{k} ' is not in experiment: ' exp_names{i}]);
+            disp(error)
+            return
+        end
+        mu(k,i) = meat{2};    
+        sigma(k,i) = meat{3};  
+        fclose(fileID);
+    end
+    
+    Corr = importdata([constraint_filedir exp_names{i} '.corr']);
     offset = 0;
     for ii=1:length(Corr)
         if Corr(ii-offset,1)==0.0
@@ -33,32 +75,18 @@ for i = 1:Num_exp
            Corr(:,ii-offset)  = [];
            offset = offset+1;
         end
-    end    
-    for k = 1:ParamDim
-        fileID = fopen(['../batch/' exp_names{i} '.margestats']);
-        Marg_header = textscan(fileID,'%s',13);
-        index(k) = 1;
-        for j = 1:200
-            meat = textscan(fileID,'%s %f %f%*[^\n]',1);
-            str =meat{1};
-            if (strcmp(str,Params{k}) == 1|strcmp(str,[Params{k},'*']) == 1)
-                break
-            end
-            index(k) = index(k)+1;
-        end
-        mu(k,i) = meat{2};    
-        sigma(k,i) = meat{3};  
-        fclose(fileID);
-    end
+    end   
     Corr_select = Corr([index],[index]);
     for n=1:ParamDim
         for m=1:ParamDim
             C(n,m,i) = sigma(n,i).*Corr_select(n,m)*sigma(m,i);
         end
     end
- end
+end
 
 
+ 
+%%%%%% calculating all the two-experiment IOIs
 IOI = zeros(Num_exp,Num_exp-1);
 for i = 1: Num_exp
     for j = (i+1):Num_exp
@@ -67,7 +95,9 @@ for i = 1: Num_exp
     end
 end
 
-fileID = fopen('IOI_CMB.txt','w');
+
+%%%%%% Saving the results in a matrix for
+fileID = fopen(Outfile,'w');
 fprintf(fileID,'%18s',' ');
 fprintf(fileID,'%18s',exp_names{:});
 for i=1:Num_exp
@@ -75,4 +105,9 @@ for i=1:Num_exp
     fprintf(fileID,'%18s',exp_names{i});
     fprintf(fileID,'%18.2f',round(IOI(i,:),2));
 end
+
+FishingMessage = sprintf(['Finish: \n' ...
+    'Two-experiment IOIs have been saved in ' Outfile '.']);
+disp(FishingMessage)
+ 
 fclose(fileID);
